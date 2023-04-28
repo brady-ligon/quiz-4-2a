06971c6b9e06eae937485c8385ca09fb5cd2d5bb
brady-ligon <brady.ligon@verizon.net>
2023-04-28 01:47:46 +0000

    Commit 1

--- sniffer.py
+++ sniffer.py
@@ -1,4 +1,5 @@
 from scapy.all import *
+import time
 
 
 def main():
@@ -9,15 +10,19 @@ def main():
         if option == '1':
             print("Creating and sending packets ...")
             # TODO
+            send_pkt(10, 1)
         elif option == '2':
             print("Listening to all traffic to 8.8.4.4 for 1 minute ...")
             # TODO
+            pkt = sniff(iface='ens4', filter='host 8.8.4.4', prn=print_pkt, timeout = 60)
         elif option == '3':
             print("Listening continuously to only ping commands to 8.8.4.4 ...")
             # TODO
+            pkt = sniff(iface='ens4', filter='icmp and host 8.8.4.4', prn=print_pkt)
         elif option == '4':
             print("Listening continuously to only outgoing telnet commands ...")
             # TODO
+            pkt = sniff(iface='ens4', filter='tcp and port 23 and src 127.0.0.1', prn=print_pkt)
         elif option == '5':
             print("End")
             break
@@ -48,6 +53,28 @@ def send_pkt(number, interval):
     """
 
     # TODO
+    for i in range(number):
+        # Define the Ethernet frame
+        eth = Ether(src='00:11:22:33:44:55', dst='55:44:33:22:11:00', type=0x0800)
+
+        # Define the IP packet
+        ip = IP(src='192.168.10.4', dst='8.8.4.4', proto='tcp', ttl = 26)
+
+        # Define the TCP segment
+        tcp = TCP(sport=23, dport=80, chksum=0)
+
+        # Define the payload
+        payload = b'RISC-V Education: https://riscvedu.org/'
+
+        # Combine everything into a single packet
+        packet = eth/ip/tcp/payload
+
+        # Send the packet
+        sendp(packet)
+
+        # Sleep interval seconds before sending next packet
+        time.sleep(interval)
+
     pass
 
 
@@ -64,6 +91,14 @@ def print_pkt(packet):
     """
 
     # TODO
+    print("Source IP:", packet[IP].src)
+    print("Destination IP:", packet[IP].dst)
+    print("Protocol number:", packet[IP].proto)
+    print("TTL:", packet[IP].ttl)
+    print("Length in bytes:", len(packet[IP]))
+    if (packet[IP].load != b''):
+        print("Raw Payload:", packet[IP].load)
+    print("\n")
     pass
 
 
